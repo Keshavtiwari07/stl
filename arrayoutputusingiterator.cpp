0f07cb55933d1da7b74c0855114a6e4f03d297c2
keshav tiwari <138362706+Keshavtiwari07@users.noreply.github.com>
2023-12-29 20:26:17 +0530

    array output iterator

---
+++ arrayoutputusingiterator.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+	
+int main() {
+    array <int ,10>arr={1,3,5,7,9,8,2,3,1,4};
+	for (auto it=arr. begin();it!=arr. end();it++){
+	    cout <<*it<<" ";//1 3 5 7 9 8 2 3 1 4 
+	}
+	return 0;
+}
