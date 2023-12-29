4ffa268d11ce0c029bd652e59826b3381d3837b4
keshav tiwari <138362706+Keshavtiwari07@users.noreply.github.com>
2023-12-29 20:00:56 +0530

    update

--- array_inputandoutput.cpp
+++ array_inputandoutput.cpp
@@ -3,10 +3,10 @@ using namespace std;
 	
 int main() {
     int arr[5];
-    for (int i=0;i<=4;i++){
+    for (int i=0;i<5;i++){
         cin>>arr[i]; //1 3 5 7 9 
    }
-	 for (int i=0;i<=4;i++){
+	 for (int i=0;i<5;i++){
         cout << arr[i]<<" ";//1 3 5 7 9 
    }
 	return 0;
--- arrayundefined.cpp
+++ arrayundefined.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 	
 int main() {
-    array <int ,5>arr={1, 3, 5};
-	 for (int i=0;i<=4;i++){
-        cout << arr.at(i)<<" ";// prints 1 3 5 0 0 
+    array <int ,10>arr={1, 3, 5};
+	 for (int i=0;i<10;i++){
+        cout << arr.at(i)<<" ";// prints 1 3 5 0 0 0 0 0 0 0
    }
 	return 0;
 }
