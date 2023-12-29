f77b916960bbd5162f0d3bd68a98906d2f1ac424
keshav tiwari <138362706+Keshavtiwari07@users.noreply.github.com>
2023-12-29 12:38:06 +0530

    container array

---
+++ containerarray_inandout.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+	
+int main() {
+    array <int ,5>arr={1, 3, 5, 7 ,9 };
+	 for (int i=0;i<=4;i++){
+        cout << arr.at(i)<<" ";//prints  1 3 5 7 9
+   }
+	return 0;
+}
