8a988c3f46e06fee31e334275b0e117965d9058c
keshav tiwari <138362706+Keshavtiwari07@users.noreply.github.com>
2023-12-29 12:41:35 +0530

    undefined array

---
+++ arrayundefined.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+	
+int main() {
+    array <int ,5>arr={1, 3, 5};
+	 for (int i=0;i<=4;i++){
+        cout << arr.at(i)<<" ";// prints 1 3 5 0 0 
+   }
+	return 0;
+}
