c67abe1bc75a38722f6517eefda59b57ed0dcc81
keshav tiwari <138362706+Keshavtiwari07@users.noreply.github.com>
2023-12-29 20:32:15 +0530

    upadate

--- arrayoutputusingiterator.cpp
+++ arrayoutputusingiterator.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
     array <int ,10>arr={1,3,5,7,9,8,2,3,1,4};
 	for (auto it=arr. begin();it!=arr. end();it++){
+       //auto it=arr.rbegin();it!=arr.rend();it++
 	    cout <<*it<<" ";//1 3 5 7 9 8 2 3 1 4 
 	}
 	return 0;
