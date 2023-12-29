db6886b03662850bb0f0d612b0a5c96646bab20f
keshav tiwari <138362706+Keshavtiwari07@users.noreply.github.com>
2023-12-29 12:05:18 +0530

    array input and output

---
+++ array_inputandoutput.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+	
+int main() {
+    int arr[5];
+    for (int i=0;i<=4;i++){
+        cin>>arr[i]; //1 3 5 7 9 
+   }
+	 for (int i=0;i<=4;i++){
+        cout << arr[i]<<" ";//1 3 5 7 9 
+   }
+	return 0;
+}
