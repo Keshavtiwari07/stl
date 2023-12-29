b011e3ff4780576e560f12b004f8ec7409ec0c87
keshav tiwari <138362706+Keshavtiwari07@users.noreply.github.com>
2023-12-29 20:43:43 +0530

    update

--- arrayoutputusingiterator.cpp
+++ arrayoutputusingiterator.cpp
@@ -4,8 +4,11 @@ using namespace std;
 int main() {
     array <int ,10>arr={1,3,5,7,9,8,2,3,1,4}; 
 	for (auto it=arr.begin();it!=arr.end();it++){// start to end will be printed 
-       //auto it=arr.rbegin();it!=arr.rend();it++   // in reverse end to start will be printed
-	    cout <<*it<<" ";//1 3 5 7 9 8 2 3 1 4 output
+    // in reverse end to start will be printed
+       //auto it=arr.rbegin();it!=arr.rend();it++   
+	      //auto it=arr.end-1;it!=arr.rbegin();it-- 
+
+           cout <<*it<<" ";//1 3 5 7 9 8 2 3 1 4 output
 	}
 	return 0;
 }
